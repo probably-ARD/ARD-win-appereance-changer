4c58d76676ee2c3dd422d38e34601c785f843245
probably-ARD <durakov.sasha95@mail.ru>
2023-06-14 19:20:47 +0300

    v0.0.0 переделан логгер (не до конца)

--- src/ARD win changer/wins/st_start_win.py
+++ src/ARD win changer/wins/st_start_win.py
@@ -22,20 +22,34 @@ class stStartWin(ctk.CTk):
         self.user_agree_caption.place(x=5, y=5)
 
         # текст самого пользовательского сочинения
-        self.user_agreement_txt = ctk.CTkTextbox(master=self, width=590, height=320)
+        self.user_agreement_txt = ctk.CTkTextbox(master=self, width=590, height=320,
+                                                 wrap='word')
         self.user_agreement_txt.grid(padx=5, pady=30, sticky='nsew')
         self.user_agreement_txt.insert('0.0', conf.USER_AGREEMENT_TXT)
         self.user_agreement_txt.configure(state='disabled')
 
         # чекбокс
         self.chekbox= ctk.CTkCheckBox(master=self, text=conf.USER_AGREEMENT_CHEKBOX_CAPTION,
-                                      width=20, height=30)
+                                      width=20, height=30, command=self.chekbox_event)
         self.chekbox.place(x=5, y=365)
 
         # кнопка продолжения
         self.btn = ctk.CTkButton(master=self, text=conf.USER_AGREEMENT_BTN_TXT,
-                                 width=20, height=30, state=ctk.DISABLED)
+                                 width=20, height=30, state=ctk.DISABLED, command=self.btn_click)
         self.btn.place(x=505, y=365)
 
+    def chekbox_event(self) -> None:
+        if self.chekbox.get() == 1:
+            self.btn.configure(state=ctk.ACTIVE)
+        else:
+            self.btn.configure(state=ctk.DISABLED)
 
-stStartWin().mainloop()
+    def btn_click(self) -> None:
+        pass
+
+
+if __name__ == '__main__':
+    try:
+        stStartWin().mainloop()
+    except Exception as exp:
+        pass
